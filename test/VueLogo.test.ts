ce0fff4c46868cfc7d3cb1c092dc1cdd4ac3256c
hojas <cyxhuan9@163.com>
2023-11-09 10:46:59 +0800

    chore(deps): upgrade deps

--- test/VueLogo.test.ts
+++ test/VueLogo.test.ts
@@ -1,15 +1,15 @@
 describe('import vue components', () => {
-  test('normal imports as expected', async () => {
+  it('normal imports as expected', async () => {
     const cmp = await import('@/pages/home/components/VueLogo.vue')
     expect(cmp).toBeDefined()
   })
 
-  test('template string imports as expected', async () => {
+  it('template string imports as expected', async () => {
     const cmp = await import('@/pages/home/components/VueLogo.vue')
     expect(cmp).toBeDefined()
   })
 
-  test('dynamic imports as expected', async () => {
+  it('dynamic imports as expected', async () => {
     const name = 'VueLogo'
     const cmp = await import((`@/pages/home/components/${name}.vue`))
     expect(cmp).toBeDefined()
